b5bcb1a9ebccc321dc79f81defadfaaf4ad4e238
joanrafaelangelperez <joanrafaelangelperez@dell-PC>
2018-06-27 23:31:45 +0000

    Added jobs.py where all peor job skills aree contained

---
+++ RSG_for_Trail_of_Chtulhu/jobs.py
@@ -0,0 +1,88 @@
+'''
+Created on Jun 27, 2018
+
+@author: joanrafaelangelperez
+'''
+
+# And another two interpersonals
+alienistProJob = [
+    'Biologia', 'Buscar libros','Evaluar sinceridad', 'Farmacologia', 
+    'Aleman', 'Latin', 'Medicina', 'Psicologia', None, None ]
+
+# And another skills whatsoever
+anticuaryProJob = [
+    'Arquitectura', 'Buscar libros', 'Derecho', 'Historia', 'Historia del arte'
+    'Latin', 'Hebreo' , 'Regatear', None]
+
+# And another two academic skills 
+arqueolegProJob = [
+    'Arqueologia', 'Atletismo', 'Buscar libros', 'Historia', 'Hebreo', 'Latin',
+    'Monta', 'Primeros auxilios', 'Recogida de pruebas', None, None]
+
+# And another two interpersonal
+artistProJob = [
+    'Adulacion', 'Arquitectura', 'Disfraz', 'Evaluar sinceridad', 'Fotografia',
+    'Habilidad artesanal', 'Habilidad artistica', 'Historia del arte', None, None]
+
+# The last two can be one of the optional academic skills below
+cientificProJob = [
+    'Buscar libros', 'Electricidad', 'Fotografia', 'Recogida de pruebas', None, None]
+cientificOptionalSkills = ['Astronomia', 'Biologia', 'Criptografia', 'Fisica', 'Geologia',
+                           'Medicina forense', 'Quimica']
+
+# And another technic skill
+criminalProJob = [
+    'Bajos fondos', 'Cerrajeria', 'Escaramuza',  'Intimidacion', 'Regatear', 'Seguir', 
+    'Sebtir el peligro', 'Sigilo']
+
+DetectiveProJob = [
+    'Armas de fuego', 'Atletismo', 'Conduccion', 'Derecho', 'Evaluar sinceridad', 
+    'Interrogatorio', 'Jerga policial', 'Recogida de pruebas', 'Sentir el peligro']
+
+# And another 5 skills whatsoever
+diletantproJob = ['Adulacion', 'Monta', 'Credito', None, None, None, None, None]
+
+# And another 3 skills whatsoever
+writterProJob = ['Buscar libros', 'Evaluar sinceridad', 'Habilidad artistica',
+                 'Historia', 'Historia oral', 'Aleman', 'Frances', None, None, None]
+
+privateProJob = [
+    'Cerrajeria', 'Conduccion', 'Consuelo', 'Contabilidad', 'Disfraz', 'Escaramuza',
+    'Evaluar sinceridad', 'Fotografia', 'Seguir']
+
+medicProJob = [
+    'Biologia', 'Consuelo', 'Contabilidad', 'Evaluar sinceridad', 'Farmacologia', 
+    'Latin', 'Medicina', 'Medicina forense', 'Primeros auxilios']
+
+priestProJob = [
+    'Buscar libros', 'Consuelo', 'Evaluar sinceridad', 'Historia', 'Latin', 'Griego',
+    'Arameo', 'Hebreo']
+
+militarProJob = [
+    'Armas', 'Armas de fuego', 'Atletismo', 'Escaramuza', 'Intimidación', 'Supervivencia', None, None, None]
+
+militaroptionalMarine = ['Conducion', 'Ocultar', 'Sigilo']
+militaroptionalCampain = ['Consuelo', 'Medicina', 'Primeros auxilios']
+militaroptionalArtill = ['Conduccion', 'Explosivos', 'Mecanica']
+militaroptionalNavy = ['Astronomia', 'Pilotaje', 'Mecanica']
+militaroptionalOficial = ['Burocracia', 'Consuelo', None]
+militaropcionalOficialoptional = ['Conduccion', 'Pilotaje']
+
+parapsicologProJob = [
+    'Antropologia', 'Buscar libros', 'Ciencias ocultas', 'Electricidad', 'Evaluar sinceridad', 
+    'Fotografia', 'Mecanica', 'Sentir el peligro']
+
+# And another interpersonal skill
+journalistProJob = [
+    'Consuelo', 'Disfraz', 'Evaluar sinceridad', 'Fotografia', 'Historia oral', 
+    'Frances', 'Aleman', 'Castellano', 'Jerga policial', 'Recogida de pruebas', 'Seguir', None]
+
+pilotProJob = [
+    'Astronomia', 'Conduccion', 'Electricidad', 'Mecanica', 'Pilotaje', 'Sentir del peligro']
+
+profesorProJob = [
+    'Burocracia', 'Frances', 'Aleman', 'Ingles', 'Castellano', 'Latin', 'Hebreo', 'Buscar libros',
+    None, None, None, None]
+
+hoboProJob = [
+    'Atletismo', 'Bajos fondos', 'Birlar', 'Regatear', 'Sentir el peligro', 'Sigilo', 'Supervivencia']
